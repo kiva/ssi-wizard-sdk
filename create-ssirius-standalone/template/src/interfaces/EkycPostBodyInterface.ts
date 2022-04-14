f781ac90354544712b40b355de44b7e21eb56676
Nate Sulat <nates@kiva.org>
2022-04-14 16:47:42 -0700

    deleting unnecessary file

--- create-ssirius-standalone/template/src/interfaces/EkycPostBodyInterface.ts
+++
@@ -1,9 +0,0 @@
-import { SearchInputData } from '../preBuilt/SearchMenu/interfaces/SearchInterfaces';
-import { AltSearchInputData } from '../preBuilt/AlternateSearch/interfaces/AltSearchInterfaces';
-
-export interface EkycPostBody {
-    search?: AltSearchInputData;
-    filters?: {
-        externalIds: SearchInputData
-    };
-}
